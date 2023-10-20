bcff1f9b7531f36ddf905f5fa7ba5241cdd333bd
owino600 <2203655@students.kcau.ac.ke>
2023-10-20 13:40:55 -0700

    python update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     else:
         print("{} argument:".format(count))
     for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} : {}".format(i + 1, sys.argv[i + 1]))
