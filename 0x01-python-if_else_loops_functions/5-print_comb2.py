989241c9329899500d92e5d8ab51b90280aaa58f
owino600 <2203655@students.kcau.ac.ke>
2023-10-22 12:08:34 -0700

    python update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for num in range(0,100):
      if number == 99:
         print("{}".format(number))
     else:
-        print("{:02}".format(number), end=", ")
+        print("{:02}".format(number), end=",")
