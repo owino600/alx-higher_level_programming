98e98bc71b79f9489a6ba0cad0cbbf199f627e6d
owino600 <owinodaniel600@gmail.com>
2024-01-13 12:46:47 +0300

    updates

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script to run msql commands
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL ON *.* TO user_0d_1@localhost
+CREATE USER IF NOT EXISTS 'user_0d_1@localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILLAGES ON *.* TO 'user_0d_1@localhost' WITH GRANT OPTION;
