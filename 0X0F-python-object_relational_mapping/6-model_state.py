409b44f1cf31422bd9e92c1cc66569c672288da9
owino600 <owinodaniel600@gmail.com>
2024-01-14 23:23:46 +0300

    updates

---
+++ 0X0F-python-object_relational_mapping/6-model_state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" start link class to table in database """
+import sys
+from model_state import Base, state
+from sqlalchemy import (create_engine)
+
+if __name__== "__main__":
+    engine = create_engine('mysql+msqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
