a5bb2cfd328be77934b06796330c5a3b1f6e8b4a
owino600 <owinodaniel600@gmail.com>
2024-01-25 18:09:17 +0300

    0-body_size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -2,9 +2,6 @@
 #Bash script that takes in a URL
 #Send request to that URL
 #Disolay the size of the body of the response
-#send request to the url
 
-URL=$1
-response=$(curl -s "$URL")
 #get the size of the response in bytes
-curl -s "$response" | wc -c
+curl -s "$1" | wc -c
