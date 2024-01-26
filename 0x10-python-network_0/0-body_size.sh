411a694c7329d437e8f4b6ba2d05378d5fe6813f
Daniel Owino <135459704+owino600@users.noreply.github.com>
2024-01-26 14:36:57 +0300

    Update 0-body_size.sh
    Update to three lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-#Bash script that takes in a URL
-#Send request to that URL
-#Disolay the size of the body of the response
-
 #get the size of the response in bytes
 curl -sI "$1" | grep -i Content-Length |cut -d " " -f 2
