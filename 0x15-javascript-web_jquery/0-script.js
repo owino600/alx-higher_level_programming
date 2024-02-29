7b27d09356efb9322ebb52bbd3be9a40eb9c5833
owino600 <owinodaniel600@gmail.com>
2024-02-29 16:48:08 +0300

    .\0-script.js

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+// Select the <header> element using document.querySelector
+const headerElement = document.querySelector("header");
+// Update the text color to red (#FF0000)
+headerElement.style.color = "#FF0000";
