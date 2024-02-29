84cf7e9407d10292bd66ffba29e12becd09644da
owino600 <owinodaniel600@gmail.com>
2024-02-29 16:51:06 +0300

    .\2-script.js

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,5 @@
+// Add a click event listener to the element with ID "red_header"
+$("#red_header").click(function() {
+    // Change the text color of the <header> element to red (#FF0000)
+    $("header").css("color", "#FF0000");
+  });  
